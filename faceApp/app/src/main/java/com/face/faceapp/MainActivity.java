05ede9f2f4c5da101ee2b298c6b23352c9af0cf3
lfchen <levon.chen@foxmail.com>
2019-06-04 22:30:00 +0800

    修改获取 referenceid时的代码错误  参考 https://github.com/Trinea

--- faceApp/app/src/main/java/com/face/faceapp/MainActivity.java
+++ faceApp/app/src/main/java/com/face/faceapp/MainActivity.java
@@ -102,6 +102,7 @@ public class MainActivity extends AppCompatActivity implements IQueryReturnHandl
         tcpClient.start();
 
 
+
         Log.d("good","");
     }
 
--- faceApp/app/src/main/java/com/face/util/DeviceRunable.java
+++ faceApp/app/src/main/java/com/face/util/DeviceRunable.java
@@ -2,10 +2,11 @@ package com.face.util;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.SharedPreferences;
 
 public class DeviceRunable  extends Thread{
 
-
+    private static String SP_DEVICES_ID = "deviceId";
     private Activity parentActivity;
 
     public DeviceRunable(Activity parent) {
@@ -18,28 +19,30 @@ public class DeviceRunable  extends Thread{
             //获取保存在sd中的 设备唯一标识符
             String readDeviceID = DeviceHelper.readDeviceID(parentActivity);
             //获取缓存在  sharepreference 里面的 设备唯一标识
-            String string = SimplePreference.getPreference(parentActivity).getString(SpConstant.SP_DEVICES_ID, readDeviceID);
+            SharedPreferences devicePreference = parentActivity.getSharedPreferences("deviceing",Context.MODE_PRIVATE);
+            String string = devicePreference.getString(SP_DEVICES_ID, readDeviceID);
             //判断 app 内部是否已经缓存,  若已经缓存则使用app 缓存的 设备id
             if (string != null) {
                 //app 缓存的和SD卡中保存的不相同 以app 保存的为准, 同时更新SD卡中保存的 唯一标识符
-                if (StringUtil.isBlank(readDeviceID) && !string.equals(readDeviceID)) {
+                if (StringUtils.isBlank(readDeviceID) && !string.equals(readDeviceID)) {
                     // 取有效地 app缓存 进行更新操作
-                    if (StringUtil.isBlank(readDeviceID) && !StringUtil.isBlank(string)) {
+                    if (StringUtils.isBlank(readDeviceID) && !StringUtils.isBlank(string)) {
                         readDeviceID = string;
                         DeviceHelper.saveDeviceID(readDeviceID, parentActivity);
                     }
                 }
             }
             // app 没有缓存 (这种情况只会发生在第一次启动的时候)
-            if (StringUtil.isBlank(readDeviceID)) {
+            if (StringUtils.isBlank(readDeviceID)) {
                 //保存设备id
                 readDeviceID = DeviceHelper.getDeviceId(parentActivity);
             }
             //左后再次更新app 的缓存
-            SimplePreference.getPreference(parentActivity).saveString(SpConstant.SP_DEVICES_ID, readDeviceID);
+            PreferencesUtils.putString(parentActivity,SP_DEVICES_ID,readDeviceID);
+
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
-    }
+    
 }
